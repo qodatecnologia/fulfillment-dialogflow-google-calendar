9fdb2c9547092fe55abcb32f471a8854be8f93e8
Qoda <39283833+qodatecnologia@users.noreply.github.com>
2019-08-02 20:09:59 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+'use strict';
+
+ const functions = require('firebase-functions');
+ const {google} = require('googleapis');
+ const {WebhookClient} = require('dialogflow-fulfillment');
+ 
+ const calendarId = "AQUI SEU ID GOOGLE CALENDAR";
+ const serviceAccount = 'AQUI VAI SEU JSON GCP'; // Começa com {"type": "service_account",...
+ 
+ // Credenciais google Calendar
+ const serviceAccountAuth = new google.auth.JWT({
+   email: serviceAccount.client_email,
+   key: serviceAccount.private_key,
+   scopes: 'https://www.googleapis.com/auth/calendar'
+ });
+ 
+ const calendar = google.calendar('v3');
+ process.env.DEBUG = 'dialogflow:*'; // enables lib debugging statements
+ 
+ const timeZone = 'America/Buenos_Aires';
+ const timeZoneOffset = '-03:00';
+ 
+ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+   const agent = new WebhookClient({ request, response });
+   console.log("Parameters", agent.parameters);
+   const appointment_type = agent.parameters.AppointmentType;
+   function makeAppointment (agent) {
+     // Calculate appointment start and end datetimes (end = +1hr from start)
+     //console.log("Parameters", agent.parameters.date);
+     const dateTimeStart = new Date(Date.parse(agent.parameters.date.split('T')[0] + 'T' + agent.parameters.time.split('T')[1].split('-')[0] + timeZoneOffset));
+     const dateTimeEnd = new Date(new Date(dateTimeStart).setHours(dateTimeStart.getHours() + 1));
+     const appointmentTimeString = dateTimeStart.toLocaleString(
+       'pt-BR',
+       { month: 'long', day: 'numeric', hour: 'numeric', timeZone: timeZone }
+     );
+ 
+     // Checa o horário e adiciona ao calendário
+     return createCalendarEvent(dateTimeStart, dateTimeEnd, appointment_type).then(() => {
+       agent.add(`Perfeito,marcado!.`);
+     }).catch(() => {
+       agent.add(`Desculpe, não consigo neste horário!`);
+     });
+   }
+ 
+   let intentMap = new Map();
+   intentMap.set('Agendamento', makeAppointment);
+   agent.handleRequest(intentMap);
+ });
+ 
+ 
+ 
+ function createCalendarEvent (dateTimeStart, dateTimeEnd, appointment_type) {
+   return new Promise((resolve, reject) => {
+     calendar.events.list({
+       auth: serviceAccountAuth, // List events for time period
+       calendarId: calendarId,
+       timeMin: dateTimeStart.toISOString(),
+       timeMax: dateTimeEnd.toISOString()
+     }, (err, calendarResponse) => {
+       // Checa se existe o evento no Google Calendar
+       if (err || calendarResponse.data.items.length > 0) {
+         reject(err || new Error('Conflito com outro agendamento!'));
+       } else {
+         // Create event for the requested time period
+         calendar.events.insert({ auth: serviceAccountAuth,
+           calendarId: calendarId,
+           resource: {summary: appointment_type +' Agendamento Dialogflow', description: appointment_type,
+             start: {dateTime: dateTimeStart},
+             end: {dateTime: dateTimeEnd}}
+         }, (err, event) => {
+           err ? reject(err) : resolve(event);
+         }
+         );
+       }
+     });
+   });
+ }
